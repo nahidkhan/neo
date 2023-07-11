203eacb9c48d3d360fea598d88a82f1bc6894338
rualrobinbiswas <rualrobin@gmail.com>
2023-07-11 14:42:04 +0600

    rual added & widget delete

---
+++ lib/rual.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Rual extends StatefulWidget {
+  const Rual({super.key});
+
+  @override
+  State<Rual> createState() => _RualState();
+}
+
+class _RualState extends State<Rual> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
